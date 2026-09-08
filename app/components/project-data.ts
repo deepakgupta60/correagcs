ea1c8f7e342c492ea0778c8d220281564bb6f678
Deepak Gupta <deepakgupta60k@gmail.com>
2026-09-08 21:32:00 +0530

    wking

--- app/components/project-data.ts
+++ app/components/project-data.ts
@@ -34,13 +34,15 @@ export const projectsData: ProjectItem[] = [
       "/projects/westhouse/8.jpeg",
     ],
 
-    video: "https://youtu.be/7iilW_Jzuak",
+    video: "https://www.youtube.com/watch?v=7iilW_Jzuak",
     location: "Keller, TX",
     date: "Decemeber 2020",
     owner: "Westside Business Holdings",
     category: "Commercial",
   },
 
+
+  
   {
     slug: "westside-corporate-hub",
     title: "Westside Corporate Hub",
