4ed584eab9b4ae232be1c32b977e2ef06d798b8c
Deepak Gupta <deepakgupta60k@gmail.com>
2026-09-17 21:00:11 +0530

    working

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,35 +63,36 @@ export default function RootLayout({
       className={`${inter.variable} ${montserrat.variable} h-full antialiased`}
     >
       <head>
-     
-     <script
-    dangerouslySetInnerHTML={{
-      __html: `
+      
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-W9BVMMLJ');
       `,
-    }}
-
-  />
-  <meta name="google-site-verification" content="zxOkPhnyasM6TEigOI_i9KSHrluhNIT4utA0wotqJBc" />
-    </head>
+          }}
+        />
+        <meta
+          name="google-site-verification"
+          content="zxOkPhnyasM6TEigOI_i9KSHrluhNIT4utA0wotqJBc"
+        />
+      </head>
       <body className="min-h-full flex flex-col">
         <noscript>
-    <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-W9BVMMLJ"
-      height="0"
-      width="0"
-      style={{ display: "none", visibility: "hidden" }}
-    />
-  </noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W9BVMMLJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <GsapAnimations />
         {children}
         <CookieBanner />
         <FloatingActions />
-        
       </body>
     </html>
   );
